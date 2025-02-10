d1937a0bb8ee4a1f66768fa51fe1d508cbecc814
Pedro Rodrigues <pedro.rodrigues@compasso.com.br>
2025-02-09 22:08:39 -0300

    chore: remove default Next.js images

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,7 @@
 export const formatText = (text: string) => {
   let formattedText = text.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
   formattedText = formattedText.replace(/\*(.*?)\*/g, "<li>$1</li>");
-
   formattedText = formattedText.replace(/\n/g, "<br />");
-
   formattedText = formattedText.replace(
     /```([\s\S]*?)```/g,
     "<pre><code>$1</code></pre>"
